286802f8cc1735674044d720c606ab815055a04b
Przesarz <przemekcesiu@gmail.com>
2026-09-18 19:54:34 +0200

    feat: configure sql server and add initial migration

---
+++ Ecommerce.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
